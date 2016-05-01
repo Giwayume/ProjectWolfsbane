6fa02e5ac0fcad0abcce17fb3769d7f2608289f5
Giwayume <giwayume@gmail.com>
2016-05-01 13:29:02 -0400

    terrain testing

---
+++ Assets/Scripts/Characters/Yvette/PlayerYvetteGroundCheck.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var player : PlayerYvette;
+
+function Start () {
+	player = gameObject.GetComponentInParent(PlayerYvette);
+}
+
+function OnTriggerEnter2D(collider) {
+	//player.grounded = true;
+}
+
+function OnTriggerStay2D(collider) {
+	player.grounded = true;
+	player.naturallyGrounded = true;
+}
+
+function OnTriggerExit2D(collider) {
+	player.grounded = false;
+	UngroundAfterTimeout();
+}
+
+function UngroundAfterTimeout() {
+	yield WaitForSeconds(0.2);
+	player.naturallyGrounded = false;
+}
---
+++ Assets/Scripts/Characters/Yvette/PlayerYvetteWallCheck.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var player : PlayerYvette;
+
+function Start () {
+	player = gameObject.GetComponentInParent(PlayerYvette);
+}
+
+function OnTriggerEnter2D(collider) {
+	player.hittingWall = true;
+}
+
+function OnTriggerStay2D(collider) {
+	player.hittingWall = true;
+}
+
+function OnTriggerExit2D(collider) {
+	player.hittingWall = false;
+}
